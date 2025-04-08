698ef65a0d2eb5f5ded27b678555ede402d07ad0
Жанатбек Әмірбек <amij@corp.ecc.kz>
2025-04-08 21:41:56 +0500

    delete time

--- time.py
+++
@@ -1,7 +0,0 @@
-import time
-from pyautogui import moveRel
-
-while True:
-    moveRel(1, 0)  # Двигаем мышь на 1 пиксель
-    moveRel(-1, 0)  # Возвращаем обратно
-    time.sleep(30)  # Пауза 30 секунд
